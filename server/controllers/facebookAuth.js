b5a8f53d42ffc791f6390d2db70f909bde6ea585
Doug Irwin <me@dougirw.in>
2016-06-09 00:27:21 -0600

    Set up Facebook backend auth

--- server/controllers/facebookAuth.js
+++ server/controllers/facebookAuth.js
@@ -1,5 +1,34 @@
+var request = require('request');
+var qs = require('querystring');
+var createSendToken = require('../utilities/jwt.js');
+var Secret = require('../config/secrets').FACEBOOK_SECRET;
+var User = require('mongoose').model('User');
 
 module.exports = function(req, res){
-    var accessTokenUrl = '';
-    var graphApiUrl = '';
+	var accessTokenUrl = 'https://graph.facebook.com/oauth/access_token';
+	var graphApiUrl = 'https://graph.facebook.com/me';
+
+    var params = {
+        client_id: req.body.clientId,
+        redirect_uri: req.body.redirectUri,
+        client_secret: Secret,
+        code: req.body.code
+    };
+
+    request.get({ url: accessTokenUrl, qs: params }, function(err, response, accessToken){
+        accessToken = qs.parse(accessToken);
+
+        request.get({ url: graphApiUrl, qs: accessToken, json: true }, function(err, response, profile){
+            User.findOne({ facebookId: profile.id}, function (err, existingUser){
+                if(existingUser) return createSendToken(existingUser, res);
+
+                var newUser = new User();
+                newUser.facebookId = profile.id;
+                newUser.displayName = profile.name;
+                newUser.save(function(err){
+                    createSendToken(newUser, res);
+                });
+            });
+        });
+    });
 }
