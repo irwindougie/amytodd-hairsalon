da770caeee6252dcfa5cccde01732f91a8d2fa30
Doug Irwin <me@dougirw.in>
2016-06-13 19:16:19 -0600

    Set Up Test Env

--- server/config/config.js
+++ server/config/config.js
@@ -8,9 +8,15 @@ var path = require('path'),
       port: process.env.PORT || 8001,
       app_url: function(){return 'http://localhost:' + this.port;}
     },
+    test: {
+      rootPath: rootPath,
+      db: process.env.Mongo_DB,
+      port: process.env.PORT || 80,
+      app_url: function(){return 'http://dev.amytodd.timefinds.com'}
+    },
     production: {
       rootPath: rootPath,
-      db: '',
+      db: process.env.Mongo_DB,
       port: process.env.PORT || 80,
       app_url: function(){return 'http://amytodd.timefinds.com'}
     }
